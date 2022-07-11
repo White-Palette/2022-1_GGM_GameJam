e827f01ca99122256356bc6950b9ce94cdae94d5
hun1001 <l01022899266@gmail.com>
2022-07-11 20:12:49 +0900

    re 제로부터 시작하는 이브렌치 생활

--- Assets/Scripts/Pillar.cs
+++ Assets/Scripts/Pillar.cs
@@ -8,69 +8,9 @@ public class Pillar : MonoBehaviour, IPoolable
     [SerializeField] Pillar leftPillar;
     [SerializeField] Pillar rightPillar;
 
-    float offset = 3f;
-
-    bool isInitUpTower = false;
-
     public void Initialize()
     {
-        TowerManager.Instance.RegisterTower(gameObject);
-        StartCoroutine(InitUpTowerCoroutine());
-    }
 
-    public void InitUpTower()
-    {
-        StartCoroutine(InitUpTowerCoroutine());
-    }
-
-    private IEnumerator InitUpTowerCoroutine()
-    {
-        yield return new WaitForSeconds(0.1f);
-        if (Vector2.Distance(Camera.main.transform.position, transform.position) > 8f)
-        {
-            yield break;
-        }
-
-        if (isInitUpTower)
-        {
-            yield break;
-        }
-
-        isInitUpTower = true;
-
-        int random = UnityEngine.Random.Range(0, 3);
-
-        Pillar leftTemp = null;
-        Pillar rightTemp = null;
-
-        switch (random)
-        {
-            case 0:
-                leftTemp = PoolManager<Pillar>.Get(TowerManager.Instance.transform);
-                break;
-            case 1:
-                rightTemp = PoolManager<Pillar>.Get(TowerManager.Instance.transform);
-                break;
-            case 2:
-                leftTemp = PoolManager<Pillar>.Get(TowerManager.Instance.transform);
-                rightTemp = PoolManager<Pillar>.Get(TowerManager.Instance.transform);
-                break;
-            default:
-                break;
-        }
-
-        if (leftTemp != null)
-        {
-            leftTemp.transform.position = transform.position + new Vector3(-offset, offset, 0);
-        }
-
-        if (rightTemp != null)
-        {
-            rightTemp.transform.position = transform.position + new Vector3(offset, offset, 0);
-        }
-
-        leftPillar = leftTemp;
-        rightPillar = rightTemp;
     }
 
     public void MoveLeft()
