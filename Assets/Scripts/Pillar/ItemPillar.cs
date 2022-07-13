cdd764370d2cdbb79a6098359c47d68f2a2d90ea
hun1001 <l01022899266@gmail.com>
2022-07-13 10:45:41 +0900

    만듬

---
+++ Assets/Scripts/Pillar/ItemPillar.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPillar : Pillar
+{
+    public override void TowerEvent()
+    {
+        // 여기서 처리
+    }
+}
