c974cb8e2e1b49861a61a8efd79df3fb797791ec
hun1001 <l01022899266@gmail.com>
2022-07-14 22:32:17 +0900

    Update Chaser.cs

--- Assets/Scripts/Guarder/Chaser.cs
+++ Assets/Scripts/Guarder/Chaser.cs
@@ -18,6 +18,7 @@ public class Chaser : MonoSingleton<Chaser>
     private void Start()
     {
         _animator = GetComponent<Animator>();
+        Debug.Log(_animator);
     }
 
     public void Eat()
@@ -44,8 +45,7 @@ public class Chaser : MonoSingleton<Chaser>
             Eat();
             VolumeController.Instance.MotionBlur = 1f;
         }
-        SetSpeed(1 / PlayerController.Instance.JumpDuration());
-        Debug.Log("Speed: " + _speed);
+        SetSpeed(2 / PlayerController.Instance.JumpDuration());
         Move();
     }
 
