fcb9a0e261630930ece66e47e5a3e01756a38304
KBluePurple <42506968+BlWon@users.noreply.github.com>
2022-07-14 14:29:05 +0900

    벚꽃 이펙트

--- Assets/Scripts/Pillar/Pillar.cs
+++ Assets/Scripts/Pillar/Pillar.cs
@@ -74,6 +74,8 @@ public abstract class Pillar : MonoBehaviour, IPoolable
         transform.DOMoveY(transform.position.y, 0.2f).From(transform.position.y - 1f);
         topSprite.DOFade(1f, 0.2f).From(0f);
         bodySprite.DOFade(1f, 0.2f).From(0f);
+        topSprite.color = SeasonManager.Instance.GetSeasonEffect()._topColor;
+        bodySprite.color = SeasonManager.Instance.GetSeasonEffect()._bodyColor;
         SeasonManager.Instance.AddTower(this);
     }
 
