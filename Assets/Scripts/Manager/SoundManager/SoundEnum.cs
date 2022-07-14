1671362f4e04fd9c58c67fcf05abe65848eab8b5
hun1001 <l01022899266@gmail.com>
2022-07-14 11:53:20 +0900

    사운드 적용

--- Assets/Scripts/Manager/SoundManager/SoundEnum.cs
+++ Assets/Scripts/Manager/SoundManager/SoundEnum.cs
@@ -1,7 +1,14 @@
 public enum Music
 {
     None,
-
+    Time_Goes_By,
+    Wonderful_Story,
+    Channel,
+    Oh_no,
+    Stellar_Crew,
+    Old_Book_Tales,
+    Wonderful_Story_Alt_Version,
+    Stellar_Crew_Alt_Version
 }
 
 public enum Effect
